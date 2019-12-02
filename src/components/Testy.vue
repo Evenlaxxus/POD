01d702b2e695385b124b2ca58d05fc582820ce0b
Evenlaxxus <r.ewiak1404@wp.pl>
2019-12-02 09:31:59 +0100

    hotfix

--- src/components/Testy.vue
+++ src/components/Testy.vue
@@ -162,6 +162,7 @@ export default {
       const reader = new FileReader();
       reader.onload = e => {
         this.input = e.target.result;
+        this.test();
       };
       reader.readAsText(file);
     },
